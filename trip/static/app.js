d0c5fb689add72ad45f786ea46a3d6906d650c2a
Givenchie <moretigiven@gmail.com>
2023-03-25 22:15:52 +0200

    Add static

--- trip/static/app.js
+++ trip/static/app.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
   const editBtn =document.querySelector("#editBtn");
+  const addTripBtn = document.querySelector("#add_trip");
+
+  
   console.log(editBtn.innerHTML);
-  if(editBtn.innerHTML === "Completed Trip"){
-    editBtn.disabled = true;
+  if(editBtn.innerHTML === "Completed Trip" || editBtn.innerHTML==="Requested Trip"){
+    editBtn.display = "None";
+  }else{
+    console.log(editBtn.innerHTML);
   }
+
+  console.log(addTripBtn.innerHTML);
+
+  // handle login form
+  const username = document.querySelector("#username");
+  console.log(username.value);
+  const password = document.querySelector("#password");
+  console.log(password.value);
+
 });
